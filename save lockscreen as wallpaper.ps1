ae8e42eac71672cc3128ff0c0c4198007dc7780e
Santiago Robledo <Santiago.Robledo@Lumen21.com>
2017-02-21 11:52:10 -0300

    added another entry to skip list

--- save lockscreen as wallpaper.ps1
+++ save lockscreen as wallpaper.ps1
@@ -9,10 +9,10 @@ $guid = [System.Guid]::NewGuid().guid
 $tempPath = "$savePath\$guid"
 New-Item $tempPath -ItemType Directory -Force | Out-Null #Create temp folder
 
-[string[]]$skip =  '8b74981a36852521d967ca3f56fea420788993d927db53b5f7cb57f6cf217e99'  #List of images to skip
+[string[]]$skip =  '8b74981a36852521d967ca3f56fea420788993d927db53b5f7cb57f6cf217e99', '390b0ea66bb431a9cfdb4ad8957e91368cee38f4de703bfd69c353f664dc9e7f'  #List of images to skip
 $assetsPath = "$($env:USERPROFILE)\AppData\Local\Packages\Microsoft.Windows.ContentDeliveryManager*\LocalState\Assets"
 
-$assets = Get-ChildItem -Path "$assetsPath\*" | Where-Object { ($_.Length -gt 100kb) -and ($skip -notcontains $_.Name) } #Get assets with more than 100kb and not in the skip list
+$assets = Get-ChildItem -Path "$assetsPath\*" | Where-Object { ($_.Length -gt 200kb) -and ($skip -notcontains $_.Name) } #Get assets with more than 200kb and not in the skip list
 
 $count = 0
 foreach($asset in $assets)
