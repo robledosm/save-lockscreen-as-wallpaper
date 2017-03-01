6f8f5195b7ea47513c10d014444cb06023cdbab6
Santiago Robledo <Santiago.Robledo@Lumen21.com>
2017-03-01 18:56:28 -0300

    Moved check for file exists to get childitem filter

--- save lockscreen as wallpaper.ps1
+++ save lockscreen as wallpaper.ps1
@@ -20,24 +20,21 @@ if (Test-Path(".\skipFiles.config")) {
 
 $assetsPath = "$($env:USERPROFILE)\AppData\Local\Packages\Microsoft.Windows.ContentDeliveryManager*\LocalState\Assets"
 
-$assets = Get-ChildItem -Path "$assetsPath\*" | Where-Object { ($_.Length -gt 200kb) -and ($skip -notcontains $_.Name) } #Get assets with more than 200kb and not in the skip list
+#Get assets with more than 200kb and not in the skip list and not already in the destination folder
+$assets = Get-ChildItem -Path "$assetsPath\*" | Where-Object { ($_.Length -gt 200kb) -and ($skip -notcontains $_.Name) -and (-not (Test-Path("$savePath\$($_.Name).png"))) } 
 
 $count = 0
 foreach($asset in $assets) {
-    $finalImagePath = "$savePath\$($asset.Name).png"
-    if (-not (Test-Path($finalImagePath))) {
-        #If the file does not exists in its final destination
-        $tempImagePath = "$tempPath\$($asset.Name).png"
-        Copy-Item $asset.FullName $tempImagePath #Copy the file to the temp folder adding .png as extension
-        $image = New-Object -comObject WIA.ImageFile
-        $image.LoadFile($tempImagePath)
-        if($image.Width.ToString() -eq "1920") {
-            #If the image is 1920 pixels width...
-            Move-Item $tempImagePath $finalImagePath -Force #Move it to its final destination
-            $count++
-        }
+    $tempImagePath = "$tempPath\$($asset.Name).png"
+    Copy-Item $asset.FullName $tempImagePath #Copy the file to the temp folder adding .png as extension
+    $image = New-Object -comObject WIA.ImageFile
+    $image.LoadFile($tempImagePath)
+    if($image.Width.ToString() -eq "1920") {
+        #If the image is 1920 pixels width...
+        Move-Item $tempImagePath "$savePath\$($asset.Name).png" -Force #Move it to its final destination
+        $count++
     }
 }
 
-Remove-Item $tempPath -Recurse #remove temp folder and files
+#Remove-Item $tempPath -Recurse #remove temp folder and files
 Write-Host "$count new pictures found"
